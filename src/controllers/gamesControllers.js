58d3a4fa0c5a014d17463b8f871068efacf0319f
Nicolas Araújo <nicolasaraujo@gmail.com>
2022-12-11 19:33:20 -0300

    feat: medelo de games

--- src/controllers/gamesControllers.js
+++ src/controllers/gamesControllers.js
@@ -1,5 +1,9 @@
 import connection from "../database/db.js";
 
-export async function getGames(req, res) {}
+export async function getGames(req, res) {
 
-export async function postGames(req, res) {}
+}
+
+export async function postGames(req, res) {
+    
+}

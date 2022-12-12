46fab2253c091667985dad054ebbe8d0fb521a13
Nicolas Araújo <nicolasaraujo@gmail.com>
2022-12-12 00:20:31 -0300

    feat: agora o front pode incerir um jogo

--- src/controllers/gamesControllers.js
+++ src/controllers/gamesControllers.js
@@ -1,9 +1,48 @@
 import connection from "../database/db.js";
+import gamesSchema from "../models/gamesSchema.js";
 
-export async function getGames(req, res) {
-
-}
+export async function getGames(req, res) {}
 
 export async function postGames(req, res) {
-    
+  const game = req.body;
+
+  try {
+    const { error } = gamesSchema.validate(game, { abortEarly: false });
+    if (error) {
+      if (error) {
+        const errors = error.details.map((detail) => detail.message);
+        return res.status(400).send(errors);
+      }
+    }
+
+    const categorieExist = await connection.query(
+      `SELECT * FROM categories WHERE id=$1;`,
+      [game.categoryId]
+    );
+    if (!categorieExist.rows[0]?.name) {
+      return res.status(400).send("Categoria não existente");
+    }
+
+    const gameExist = await connection.query(
+      `SELECT name FROM games WHERE name ILIKE '%'||$1||'%';`,
+      [game.name]
+    );
+    if (gameExist) return res.status(409).send("Jogo com nome já existente");
+
+    await connection.query(
+      `INSERT INTO games (name, image, "stockTotal", "categoryId", "pricePerDay") VALUES ($1, $2, $3, $4, $5);`,
+      [
+        game.name,
+        game.image,
+        game.stockTotal,
+        game.categoryId,
+        game.pricePerDay,
+      ]
+    );
+
+    res.sendStatus(201);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
 }
--- src/models/gamesSchema.js
+++ src/models/gamesSchema.js
@@ -3,8 +3,8 @@ import joi from "joi";
 const gamesSchema = joi.object({
   name: joi.string().min(3).required(),
   image: joi.string().uri().required(),
-  stockTotal: joi.number().min(0).required(),
-  categoryId: joi.number().min(0).required(),
+  stockTotal: joi.number().min(1).required(),
+  categoryId: joi.number().min(1).required(),
   pricePerDay: joi.number().min(1).required(),
 });
 

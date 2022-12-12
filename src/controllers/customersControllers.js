2b9466f845274caf4e18766715f92f7198d6b2e4
Nicolas Araújo <nicolasaraujo@gmail.com>
2022-12-12 19:50:45 -0300

    refactor: modelo de cliente

--- src/controllers/customersControllers.js
+++ src/controllers/customersControllers.js
@@ -1,9 +1,25 @@
 import connection from "../database/db.js";
+import customersSchema from "../models/customersSchema.js";
 
 export async function getCustomers(req, res) {}
 
 export async function postCustomers(req, res) {
+  const customer = req.body;
+
+  try {
+    const { error } = customersSchema.validate(customer, { abortEarly: false });
+    if (error) {
+      const errors = error.details.map((detail) => detail.message);
+      return res.status(400).send(errors);
+    }
+
     
+
+    res.sendStatus(201);
+  } catch (err) {
+    res.sendStatus(500);
+    console.log(err);
+  }
 }
 
 export async function putCustomers(req, res) {}
--- src/models/customersSchema.js
+++ src/models/customersSchema.js
@@ -1,9 +1,9 @@
-import joi, { string } from "joi";
+import joi from "joi";
 
 const customersSchema = joi.object({
-  name: joi.string().min(),
-  phone: joi.string().min(10).max(11).alphanum(0-9).required(),
-  cpf: joi.string().min(11).required(),
+  name: joi.string().min(2).required(),
+  phone: joi.string().min(10).max(11).trim().pattern(/^[0-9]+$/, 'numbers').required(),
+  cpf: joi.string().length(11).trim().pattern(/^[0-9]+$/, 'numbers').required(),
   birthday: joi.date().less("now").required(),
 });
 

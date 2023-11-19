de0be25899af9318417d4a8eeef7752b1db5762b
Nicolas Araújo <nicolasaraujo@gmail.com>
2023-11-19 09:37:24 -0300

    feat: adicionado script de inicialização da aplicação e rota de saúde da aplicação

--- src/server.js
+++ src/server.js
@@ -1,5 +1,4 @@
 import express from "express";
-import dotenv from "dotenv";
 import cors from "cors";
 
 import categoriesRouters from "./routers/categoriesRouters.js";
@@ -7,11 +6,11 @@ import gamesRouters from "./routers/gamesRouters.js";
 import customersRouters from "./routers/customersRouters.js";
 import rentalsRouters from "./routers/rentalsRouters.js";
 
-dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get("/health", (req, res) => res.send("API funcionando corretamente."))
 app.use(categoriesRouters);
 app.use(gamesRouters);
 app.use(customersRouters);
